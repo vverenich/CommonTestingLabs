d90aef8523219511f11c017a6a58c9dca4570bb4
dxarch <emilepatch@gmail.com>
2021-10-27 11:53:19 +0300

    added lab_3

---
+++ lab_3/MinApp/TestMinApp/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using MinApp;
+
+namespace TestMinApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMin()
+        {
+            int min;
+            min = Program.Min(3, 4, 5);
+            Assert.AreEqual(3, min);
+        }
+    }
+}
